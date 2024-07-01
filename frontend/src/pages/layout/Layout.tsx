76556a72333192236564815034a59d873247c8ed
bridodonnellesb <contractor.brid.odonnell@esb.ie>
2024-07-01 15:58:23 +0100

    copied from cs bot

---
+++ frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,169 @@
+import { Outlet, Link } from "react-router-dom";
+import styles from "./Layout.module.css";
+import EI from "../../assets/EI.svg";
+import { CopyRegular } from "@fluentui/react-icons";
+import { Dialog, Stack, TextField, Dropdown, IDropdownOption } from "@fluentui/react";
+import { useContext, useEffect, useState } from "react";
+import { HistoryButton, ShareButton } from "../../components/common/Button";
+import { AppStateContext } from "../../state/AppProvider";
+import { CosmosDBStatus } from "../../api";
+
+const Layout = () => {
+    const [isSharePanelOpen, setIsSharePanelOpen] = useState<boolean>(false);
+    const [copyClicked, setCopyClicked] = useState<boolean>(false);
+    const [copyText, setCopyText] = useState<string>("Copy URL");
+    const [shareLabel, setShareLabel] = useState<string | undefined>("Share");
+    const [hideHistoryLabel, setHideHistoryLabel] = useState<string>("Hide chat history");
+    const [showHistoryLabel, setShowHistoryLabel] = useState<string>("Show chat history");
+    const appStateContext = useContext(AppStateContext)
+    const ui = appStateContext?.state.frontendSettings?.ui;
+
+    const handleShareClick = () => {
+        setIsSharePanelOpen(true);
+    };
+
+    const handleSharePanelDismiss = () => {
+        setIsSharePanelOpen(false);
+        setCopyClicked(false);
+        setCopyText("Copy URL");
+    };
+
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(window.location.href);
+        setCopyClicked(true);
+    };
+
+    const handleHistoryClick = () => {
+        appStateContext?.dispatch({ type: 'TOGGLE_CHAT_HISTORY' })
+    };
+
+    useEffect(() => {
+        if (copyClicked) {
+            setCopyText("Copied URL");
+        }
+    }, [copyClicked]);
+
+    useEffect(() => { }, [appStateContext?.state.isCosmosDBAvailable.status]);
+
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth < 480) {
+            setShareLabel(undefined)
+            setHideHistoryLabel("Hide history")
+            setShowHistoryLabel("Show history")
+          } else {
+            setShareLabel("Share")
+            setHideHistoryLabel("Hide chat history")
+            setShowHistoryLabel("Show chat history")
+          }
+        };
+    
+        window.addEventListener('resize', handleResize);
+        handleResize();
+    
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+
+    const dropdownOptions = [
+        { key: 'CRM7', text: 'CRM7' },
+        { key: 'SRP', text: 'SRP' },
+        { key: 'IRP', text: 'IRP' }
+    ];
+
+    const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
+
+    const onDropdownChange = (
+        event: React.FormEvent<HTMLDivElement>,
+        option?: IDropdownOption, // option is now optional and matches IDropdownOption type
+        index?: number // index is optional and of type number
+    ): void => {
+        if (option) {
+            const newSelectedKeys = option.selected
+                ? [...selectedKeys, option.key as string]
+                : selectedKeys.filter(key => key !== option.key);
+
+            // Update the local state
+            setSelectedKeys(newSelectedKeys);
+
+            // Dispatch the action to update the AppStateContext
+            appStateContext?.dispatch({
+                type: 'UPDATE_SELECTED_OPTIONS',
+                payload: newSelectedKeys,
+            });
+        }
+    };
+
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
+                    <Stack horizontal verticalAlign="center">
+                        <img
+                            src={ui?.logo ? ui.logo : EI}
+                            className={styles.headerIcon}
+                            aria-hidden="true"
+                        />
+                        <Link to="/" className={styles.headerTitleContainer}>
+                            <h1 className={styles.headerTitle}>{ui?.title}</h1>
+                        </Link>
+                    </Stack>
+                    <Stack horizontal tokens={{ childrenGap: 10 }} className={styles.shareButtonContainer}>
+                        <Dropdown
+                            placeholder="Select system to filter by"
+                            multiSelect
+                            options={dropdownOptions}
+                            selectedKeys={selectedKeys}
+                            onChange={onDropdownChange}
+                            styles={{ dropdown: { width: 250 } }} // Adjust width as needed
+                        />
+                        {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) &&
+                            <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? hideHistoryLabel : showHistoryLabel} />
+                        }
+                        {ui?.show_share_button &&<ShareButton onClick={handleShareClick} text={shareLabel} />}
+                    </Stack>
+                </Stack>
+            </header>
+            <Outlet />
+            <Dialog
+                onDismiss={handleSharePanelDismiss}
+                hidden={!isSharePanelOpen}
+                styles={{
+
+                    main: [{
+                        selectors: {
+                            ['@media (min-width: 480px)']: {
+                                maxWidth: '600px',
+                                background: "#FFFFFF",
+                                boxShadow: "0px 14px 28.8px rgba(0, 0, 0, 0.24), 0px 0px 8px rgba(0, 0, 0, 0.2)",
+                                borderRadius: "8px",
+                                maxHeight: '200px',
+                                minHeight: '100px',
+                            }
+                        }
+                    }]
+                }}
+                dialogContentProps={{
+                    title: "Share the web app",
+                    showCloseButton: true
+                }}
+            >
+                <Stack horizontal verticalAlign="center" style={{ gap: "8px" }}>
+                    <TextField className={styles.urlTextBox} defaultValue={window.location.href} readOnly />
+                    <div
+                        className={styles.copyButtonContainer}
+                        role="button"
+                        tabIndex={0}
+                        aria-label="Copy"
+                        onClick={handleCopyClick}
+                        onKeyDown={e => e.key === "Enter" || e.key === " " ? handleCopyClick() : null}
+                    >
+                        <CopyRegular className={styles.copyButton} />
+                        <span className={styles.copyButtonText}>{copyText}</span>
+                    </div>
+                </Stack>
+            </Dialog>
+        </div>
+    );
+};
+
+export default Layout;
