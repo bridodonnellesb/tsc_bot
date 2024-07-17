7c7b51cdd99537b287a87d2a21f238432c3e6ed2
bridodonnellesb <contractor.brid.odonnell@esb.ie>
2024-07-17 17:02:40 +0100

    Added generating SAS for pdf preview

--- backend/history/cosmosdbservice.py
+++ backend/history/cosmosdbservice.py
@@ -1,7 +1,14 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
 from azure.cosmos.aio import CosmosClient
 from azure.cosmos import exceptions
+import json
+from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
+import os
+import re
+
+BLOB_CREDENTIAL = os.environ.get("BLOB_CREDENTIAL")
+BLOB_ACCOUNT = os.environ.get("BLOB_ACCOUNT")
   
 class CosmosConversationClient():
     
@@ -177,7 +184,36 @@ class CosmosConversationClient():
         ]
         query = f"SELECT * FROM c WHERE c.conversationId = @conversationId AND c.type='message' AND c.userId = @userId ORDER BY c.timestamp ASC"
         messages = []
+        blob_service_client =BlobServiceClient(BLOB_ACCOUNT, credential=BLOB_CREDENTIAL)
+        def split_url(url):
+            pattern = fr'{BLOB_ACCOUNT}/([\w-]+)/([\w-]+)\.pdf'
+            match = re.search(pattern, url)
+            container = match.group(1)
+            blob = match.group(2)
+            return container, blob
+
+        def generate_SAS(url):
+            container, blob = split_url(url)
+            blob_client = blob_service_client.get_blob_client(container=container, blob=blob)
+        
+            sas_token_expiry_time = datetime.utcnow() + timedelta(hours=1)  # 1 hour from now
+        
+            sas_token = generate_blob_sas(
+                account_name=blob_client.account_name,
+                container_name=blob_client.container_name,
+                blob_name=blob_client.blob_name,
+                account_key=BLOB_CREDENTIAL,
+                permission=BlobSasPermissions(read=True),
+                expiry=sas_token_expiry_time
+            )
+        
+            return sas_token
         async for item in self.container_client.query_items(query=query, parameters=parameters):
+            if item.role=="tool":
+                content = json.loads(item.content)
+                for i, chunk in enumerate(content["citations"]):
+                    content["citations"][i]["url"]=chunk["url"]+"?"+generate_SAS(chunk["url"])
+                item.content = json.dumps(content)
             messages.append(item)
 
         return messages
