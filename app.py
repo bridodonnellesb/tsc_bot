36f40d003a5f22ff0a55fff0c60443167820b9a7
bridodonnellesb <contractor.brid.odonnell@esb.ie>
2024-07-25 10:28:43 +0100

    logic fix for empty filtered formulas array

--- app.py
+++ app.py
@@ -1576,9 +1576,13 @@ async def get_formula():
                         screenshot_formula(image_bytes, formula["content"], combined_polygon)
                         error = "screenshot_formula"
 
-                error="screenshots saved"
-                for i, formula in enumerate(filtered_formulas):
-                    sorted_array = insert_in_reading_order(words, formula)
+                error=f"number of formulas screenshots saved - {filtered_formulas}"
+                if len(filtered_formulas)>0:
+                    for i, formula in enumerate(filtered_formulas):
+                        error = str(formula)
+                        sorted_array = insert_in_reading_order(words, formula)
+                else:
+                    sorted_array = words
 
                 offsets = []
                 formulas_output = []
