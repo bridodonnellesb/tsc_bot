f92b552d1b157a8056afef86b112433a89f1c0e3
bridodonnellesb <contractor.brid.odonnell@esb.ie>
2024-08-16 16:21:16 +0100

    Revert "parse and replace the separate blob containers"
    This reverts commit 80a3af94580955f2b613e6ac29a3d5bb5350b9c5.

--- frontend/src/components/Answer/AnswerParser.tsx
+++ frontend/src/components/Answer/AnswerParser.tsx
@@ -20,25 +20,12 @@ export const enumerateCitations = (citations: Citation[]) => {
     return citations;
 }
 
-const splitAndReplace = (url: string | null) => {
-    const BLOB_ACCOUNT = 'https://datascienceteampocra7fd.blob.core.windows.net'; // Replace with your actual blob account name
-    const pattern = new RegExp(`${BLOB_ACCOUNT}/([\\w-]+)/([\\w-]+\\.\\w+)`);
-    if (url !== null) {
-        const match = url.match(pattern);
-        if (match) {
-        const container = match[1];
-        url = url.replace(container, "trading-bot")
-        }
-    }
-    return url; // or throw an error if you prefer
-  };
-
 export function parseAnswer(answer: AskResponse): ParsedAnswer {
     let answerText = answer.answer;
     const citationLinks = answerText.match(/\[(doc\d\d?\d?)]/g);
 
     const lengthDocN = "[doc".length;
-    
+
     let filteredCitations = [] as Citation[];
     let citationReindex = 0;
     citationLinks?.forEach(link => {
@@ -48,7 +35,6 @@ export function parseAnswer(answer: AskResponse): ParsedAnswer {
         if (!filteredCitations.find((c) => c.id === citationIndex) && citation) {
             answerText = answerText.replaceAll(link, ` ^${++citationReindex}^ `);
             citation.id = citationIndex;
-            citation.url = splitAndReplace(citation.url)
             citation.reindex_id = citationReindex.toString();
             let content = citation.content.split("\n")
             citation.content = content[0]
