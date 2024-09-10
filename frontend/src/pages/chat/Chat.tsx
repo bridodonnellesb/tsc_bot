6d632386db7fbf4528d1ad64426d326ad42df7ee
bridodonnellesb <contractor.brid.odonnell@esb.ie>
2024-09-10 10:14:24 +0100

    position relative iframe fix

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -777,7 +777,9 @@ const Chat = () => {
                                 <IconButton iconProps={{ iconName: 'Cancel' }} aria-label="Close citations panel" onClick={() => setIsCitationPanelOpen(false)} />
                             </Stack>
                             <h5 className={styles.citationPanelTitle} tabIndex={0} title={activeCitation.url ? activeCitation.url : activeCitation.title ?? ""} onClick={() => onViewSource(activeCitation)}>{activeCitation.title}</h5>
-                            <iframe className={styles.iframe} key={iframeState} src={activeCitation.url+"#page="+activeCitation.page} width="100%" height="100%"></iframe>
+                            <div className={styles.iframe}>
+                                <iframe key={iframeState} src={activeCitation.url+"#page="+activeCitation.page} width="100%" height="100%"></iframe>
+                            </div>
                             {/* <div tabIndex={0}>
                                 <ReactMarkdown
                                     linkTarget="_blank"
