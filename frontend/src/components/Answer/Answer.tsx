479be87e0612833c643988cc86d77b4dcbcd183f
bridodonnellesb <contractor.brid.odonnell@esb.ie>
2024-10-03 11:15:23 +0100

    changed . to / in filter description

--- frontend/src/components/Answer/Answer.tsx
+++ frontend/src/components/Answer/Answer.tsx
@@ -189,13 +189,13 @@ export const Answer = ({
         partsFilter: string[]
     ): string => {
         const filters = [];
-        if (rulesFilter.length > 0) filters.push(`${rulesFilter.join(', ')} for the Rules Set`);
-        if (typesFilter.length > 0) filters.push(`${typesFilter.join(', ')} for Document Type`);
+        if (rulesFilter.length > 0) filters.push(`${rulesFilter.join(' / ')} for the Rules Set`);
+        if (typesFilter.length > 0) filters.push(`${typesFilter.join(' / ')} for Document Type`);
         if (partsFilter.length > 0) {
             // Filter out "NA" from the partsFilter array
             const filteredPartsFilter = partsFilter.filter(part => part !== "NA");
             if (filteredPartsFilter.length > 0) {
-                filters.push(`${filteredPartsFilter.join(', ')} for the Trading Settlement Code Part`);
+                filters.push(`${filteredPartsFilter.join(' / ')} for the Trading Settlement Code Part`);
             }
         }
         return filters.length > 0 ? `Selected Filters: \n${filters.join(';\n')}` : 'No filters active';
